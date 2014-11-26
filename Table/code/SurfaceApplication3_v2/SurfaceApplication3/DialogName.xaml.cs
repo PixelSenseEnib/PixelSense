0d62a2a9d8c9beb6010ebe5bb9f3c4ad8c44bad0
Thibault <t2riou@enib.fr>
2014-11-26 10:17:19 +0100

    last version of 42cards

---
+++ Table/code/SurfaceApplication3_v2/SurfaceApplication3/DialogName.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SurfaceApplication3
+{
+    /// <summary>
+    /// Logique d'interaction pour Window1.xaml
+    /// </summary>
+    public partial class DialogName : Window
+    {
+                public DialogName()
+                {
+                        InitializeComponent();
+                        lblQuestion.Content = "Entrez votre nom";
+                        txtAnswer.Text = "Sebastien Kubicki";
+                        lblErreur.Content = "";
+                }
+
+                private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+                {
+                        int n;
+                        bool isNumeric = int.TryParse(txtAnswer.Text, out n);
+                        if (isNumeric)
+                            lblErreur.Content = "Tu t'appelles " + txtAnswer.Text + " ?";
+                        else
+                            this.DialogResult = true;
+                }
+
+                private void Window_ContentRendered(object sender, EventArgs e)
+                {
+                        txtAnswer.SelectAll();
+                        txtAnswer.Focus();
+                }
+
+                public string Answer
+                {
+                        get { return txtAnswer.Text; }
+                }
+        }
+    }
+
